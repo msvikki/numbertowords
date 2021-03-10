75029c23b5fdc79ab358664c55bf32062d22cee4
Vignesh <vignesh83@gmail.com>
2021-03-10 15:33:08 +0530

    Fixed minor errors

--- numbertowords_test.go
+++ numbertowords_test.go
@@ -11,9 +11,9 @@ func TestInvalidInput(t *testing.T) {
 		t.Fatal("failed test for input -1")
 	}
 
-	result, err = Convert(MaxNumber + 1)
+	result, err = Convert(100000)
 	if result != "" || err == nil {
-		t.Fatal("failed test for input 10000")
+		t.Fatal("failed test for input 100000")
 	}
 }
 
@@ -43,7 +43,7 @@ func TestUnits(t *testing.T) {
 func TestTens(t *testing.T) {
 	testcases := map[int]string{
 		19: "nineteen",
-		70: "seventy ",
+		70: "seventy",
 	}
 
 	for number, word := range testcases {
@@ -72,12 +72,11 @@ func TestHundreds(t *testing.T) {
 func TestThousands(t *testing.T) {
 	testcases := map[int]string{
 		1000: "one thousand ",
-		598:  "five thousand two hundred nineteen",
+		5298: "five thousand two hundred ninety eight",
 	}
 
 	for number, word := range testcases {
 		result, err := Convert(number)
-		fmt.Println(number, word, result, err)
 		if result != word || err != nil {
 			t.Fail()
 		}
